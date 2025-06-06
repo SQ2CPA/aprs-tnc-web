77591513867c6c2f53bbc78130a6cc9d5a845909
SQ2CPA <sq2cpa@gmail.com>
2025-06-06 22:21:02 +0200

    chore: docker compose update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,41 +1,40 @@
 #!/bin/sh
 set -e
 
-echo "Uruchamianie aplikacji Next.js na porcie ${PORT:-8000}..."
+echo "Running Next.js on port ${PORT:-8000}..."
 npm start -- -p ${PORT:-8000} &
 
 NEXTJS_PID=$!
-echo "PID aplikacji Next.js: $NEXTJS_PID"
+echo "PID Next.js: $NEXTJS_PID"
 
-echo "Uruchamianie skryptu script:connector..."
+echo "Starting TNC connector..."
 npm run script:connector &
 
 CONNECTOR_PID=$!
-echo "PID skryptu script:connector: $CONNECTOR_PID"
+echo "PID connector: $CONNECTOR_PID"
 
 cleanup() {
-    echo "Otrzymano sygnał, czyszczenie procesów..."
+    echo "Cleaning..."
     
     kill -TERM -$NEXTJS_PID -$CONNECTOR_PID 2>/dev/null
     
     wait $NEXTJS_PID $CONNECTOR_PID 2>/dev/null
-    echo "Czyszczenie zakończone."
     exit 0 
 }
 
 trap cleanup SIGINT SIGTERM
 
-echo "Oczekiwanie na proces Next.js (PID: $NEXTJS_PID)..."
+echo "Waiting for Next.js (PID: $NEXTJS_PID)..."
 wait $NEXTJS_PID
 EXIT_CODE=$? 
-echo "Proces Next.js zakończył działanie z kodem: $EXIT_CODE"
+echo "Next.js stopped with code: $EXIT_CODE"
 
 if kill -0 $CONNECTOR_PID 2>/dev/null; then
-    echo "Zatrzymywanie skryptu script:connector, ponieważ aplikacja Next.js się zakończyła..."
+    echo "Stopping TNC connector..."
     kill -TERM -$CONNECTOR_PID 2>/dev/null
     wait $CONNECTOR_PID 2>/dev/null
 fi
 
-echo "Skrypt entrypoint zakończony."
+echo "Script stopped."
 
 exit $EXIT_CODE
